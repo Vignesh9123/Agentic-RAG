aa45bebc4f2b2798722b98be0ba3fb25aacd9d17
Vignesh D <vignesh.d9123@gmail.com>
2026-04-17 14:49:28 +0530

    Add test for generation chain

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
+load_dotenv()
 from retriever import retriever
 from graph.chains.retrieval_grader import GradeDocuments, grader_chain
+from graph.chains.generation import generation_chain
 
 def test_relevancy_yes():
     question = "What are the types of memory?"
@@ -26,7 +29,20 @@ def test_relevancy_no():
         print("NEGATIVE TEST SUCCESSFUL")
     else:
         print("NEGATIVE TEST FAILED")
+    
+def test_generation():
+    question = "What are the types of Agent memory?"
+    relevant_docs = retriever.invoke(question)
+    doc_text = "\n".join([doc.page_content for doc in relevant_docs])
+    response = generation_chain.invoke({
+        "question":question,
+        "context":doc_text
+    })
+    print("GENERATION TEST SUCCESSFUL")
+
+
 
 
 test_relevancy_yes()
 test_relevancy_no()
+test_generation()
