979d2739bb51415c9cb843c71fcaa343f19e8252
Vignesh D <vignesh.d9123@gmail.com>
2026-04-17 14:49:44 +0530

    Add generation node

---
+++ graph/nodes/generate.py
@@ -0,0 +1,16 @@
+from graph.chains.generation import generation_chain
+from graph.state import GraphState
+
+def generate(state: GraphState) -> GraphState:
+    question = state['question']
+    documents = state['documents']
+
+    context = "\n".join(doc.page_content for doc in documents)
+
+    generated_content = generation_chain.invoke({
+        "question":question,
+        "context":context
+    })
+    return {"generated_content": generated_content}
+
+
